5d3c4e74d394fd18612baeb485b48001486312eb
softcafecommon <softcafe.common@gmail.com>
2023-12-20 16:44:41 +0600

    home m3

--- lib/value/all_dynamic_value.dart
+++ lib/value/all_dynamic_value.dart
@@ -10,7 +10,7 @@ class AllDynamicValue {
   final Color? appHomeColor = Colors.blue[800];
   final Color? appHomed1 = const Color.fromARGB(80, 19, 78, 146);
   final Color? appHomed2 = const Color.fromARGB(118, 19, 78, 146);
-  final Color? appHomeShadow = Color.fromARGB(54, 0, 0, 0);
+  final Color? appHomeShadow = const Color.fromARGB(54, 0, 0, 0);
   final Color? appSidebarColor = const Color.fromARGB(255, 28, 0, 129);
 
   getHeight(context) {
